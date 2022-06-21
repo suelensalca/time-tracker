e6cc588d1091b875eb7586a73f3ed3cd7250b90b
suelen <suelen@macbook.local>
2022-06-21 17:44:50 -0300

    cards layout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ export function App() {
   return <main className={styles.container}>
     <User/>
     <div className={styles.cardContainer}>
-      <Card/>
+    <Card icon={'../../../icon-work.svg'} activity={"Work"} hours={"36h"}/>
+    <Card icon={'../../../icon-play.svg'} activity={"Play"} hours={"18h"}/>
+    <Card icon={'../../../icon-study.svg'} activity={"Study"} hours={"9h"}/>
+    <Card icon={'../../../icon-exercise.svg'} activity={"Exercise"} hours={"4h"}/>
+    <Card icon={'../../../icon-social.svg'} activity={"Social"} hours={"2h"}/>
+    <Card icon={'../../../icon-self-care.svg'} activity={"Self Care"} hours={"1h"}/>
     </div>
   </main>
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,22 @@
 import styles from './styles.module.scss'
 
-export function Card() {
+export function Card({icon, activity, hours }) {
     return (
         <div className={styles.container}>
-            <div className={styles.header}>
-                <img src='../../../icon-work.svg'/>
+            <div className={
+                activity === "Work" ? [styles.header, styles.work].join(" ") : 
+                activity === "Play" ? [styles.header, styles.play].join(" ") : 
+                activity === "Study" ? [styles.header, styles.study].join(" ") : 
+                activity === "Exercise" ? [styles.header, styles.exercise].join(" ") : 
+                activity === "Social" ? [styles.header, styles.social].join(" ") : 
+                activity === "Self Care" ? [styles.header, styles.selfCare].join(" ") : 
+                styles.header}>
+                <img src={icon} alt={activity}/>
             </div>
             <div className={styles.info}>
-                <p className={styles.activity}>Trabalho</p>
-                <p className={styles.hours}>32h</p>
-                <p className={styles.pastHours}>Semana Passada - 36h</p>
+                <p className={styles.activity}>{activity}</p>
+                <p className={styles.hours}>{hours}</p>
+                <p className={styles.pastHours}>Semana Passada - {hours}</p>
             </div>
         </div>
     )
