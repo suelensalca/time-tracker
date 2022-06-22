b84606dba4580e9e5a2485ff5afbbb69049c04de
suelen <suelen@macbook.local>
2022-06-22 15:29:36 -0300

    images files

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ export function App() {
   return <main className={styles.container}>
     <User/>
     <div className={styles.cardContainer}>
-    <Card icon={'../../../icon-work.svg'} activity={"Trabalho"} hours={"36h"}/>
-    <Card icon={'../../../icon-play.svg'} activity={"Diversão"} hours={"18h"}/>
-    <Card icon={'../../../icon-study.svg'} activity={"Estudo"} hours={"9h"}/>
-    <Card icon={'../../../icon-exercise.svg'} activity={"Exercício"} hours={"4h"}/>
-    <Card icon={'../../../icon-social.svg'} activity={"Social"} hours={"2h"}/>
-    <Card icon={'../../../icon-self-care.svg'} activity={"Autocuidado"} hours={"1h"}/>
+    <Card icon={'../../../images/icon-work.svg'} activity={"Trabalho"} hours={"36h"}/>
+    <Card icon={'../../../images/icon-play.svg'} activity={"Diversão"} hours={"18h"}/>
+    <Card icon={'../../../images/icon-study.svg'} activity={"Estudo"} hours={"9h"}/>
+    <Card icon={'../../../images/icon-exercise.svg'} activity={"Exercício"} hours={"4h"}/>
+    <Card icon={'../../../images/icon-social.svg'} activity={"Social"} hours={"2h"}/>
+    <Card icon={'../../../images/icon-self-care.svg'} activity={"Autocuidado"} hours={"1h"}/>
     </div>
   </main>
 }
--- src/components/User/index.js
+++ src/components/User/index.js
@@ -4,7 +4,7 @@ export function User() {
     return (
         <div className={styles.container}>
             <div className={styles.header}>
-                <img src='../../../image-jeremy.png' alt="user"/>
+                <img src='../../../images/image-jeremy.png' alt="user"/>
                 <p>Jeremy Robson</p>
             </div>
             <div className={styles.timePeriod}>
