ddfd68bf619217c6890e2698a353fd319dc6c849
suelen <suelen@macbook.local>
2022-06-23 20:16:44 -0300

    passport js integration

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const cookieSession = require("cookie-session");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const app = express();
+
+app.use(cookieSession(
+    {name:"session",
+    keys:["lama"],
+    maxAge: 24 * 60 * 60 * 100 //1 day
+}
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: "GET, POST, PUT, DELETE",
+        credentials: true,
+    })
+);
+
+app.listen("5000", () => { 
+    console.log("Server is runing!")
+})
