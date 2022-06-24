8558b286386c1351b1d19a1cfb10da430d66bff4
suelen <suelen@macbook.local>
2022-06-24 10:52:38 -0300

    github social login

--- front/src/components/User/index.js
+++ front/src/components/User/index.js
@@ -1,12 +1,57 @@
 import styles from './styles.module.scss'
+import { useEffect, useState } from "react";
 
 export function User() {
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const getUser = () => {
+            fetch("http://localhost:5000/auth/login/success", {
+                method: "GET",
+                credentials: "include",
+                headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Credentials": true,
+                },
+            }).then((response) => {
+                if (response.status === 200) return response.json();
+                throw new Error("authentication has been failed!");
+            }).then((resObject) => {
+                setUser(resObject.user);
+            }).catch((err) => {
+                console.log(err);
+            });
+        };
+        getUser();
+    }, []);
+
+    console.log(user);
+
+    const github = () => {
+        window.open("http://localhost:5000/auth/github", "_self")
+    };
+
+    const logout = () => {
+        window.open("http://localhost:5000/auth/logout", "_self");
+    };
+
     return (
         <div className={styles.container}>
-            <div className={styles.header}>
-                <img src='../../../images/image-jeremy.png' alt="user"/>
-                <p>Jeremy Robson</p>
-            </div>
+                {user ? (
+                    <div className={styles.header}>
+                        <img src={user.photos[0].value} alt={user.displayName} />
+                        <p>{user.displayName}</p>
+                        <button onClick={logout}>Logout</button>
+                    </div>
+                ) : (
+                    <div className={styles.header} onClick={github}>
+                        <img src='../../../images/image-jeremy.png' alt="user"/>
+                        <p>Usuário</p>
+                        <button onClick={github}>Login</button>
+                    </div>
+                )}
             <div className={styles.timePeriod}>
                 <button>Diário</button>
                 <button>Semanal</button>
